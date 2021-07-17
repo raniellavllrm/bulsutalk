46e52bb33add3f5da024b8f83cdb84bea91d2696
raniellavllrm <vraniella@gmail.com>
2021-07-17 18:22:11 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -105,7 +105,7 @@ export default function Home() {
       })
       .then((doc) => {
         post.postID = doc.id;
-        handleUpload(post.postID);
+        handleUpload(doc.id);
       });
     post.postContent = "";
   };
